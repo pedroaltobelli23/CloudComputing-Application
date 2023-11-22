85d14a7d824d640a861bd45bb031b9155bf3fedf
pedroaltobelli23 <pedroaltobelli673@gmail.com>
2023-11-22 19:45:55 -0300

    health check

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from application import views
 from django.contrib import admin
+from .views import health_check
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,5 +12,6 @@ urlpatterns = [
     # path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin',admin.site.urls),
+    path('health/',health_check),
     path('',include('application.urls'))
 ]
