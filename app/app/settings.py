61aa8c972daff744e6826361ad14c89fcbe16c43
pedroaltobelli23 <pedroaltobelli673@gmail.com>
2023-11-25 15:23:23 -0300

    aa

--- app/app/settings.py
+++ app/app/settings.py
@@ -114,4 +114,12 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
+# ALLOWED_HOSTS = ['127.0.0.1']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
--- app/app/urls.py
+++ app/app/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     # path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin',admin.site.urls),
-    path('health/',health_check),
+    # path('health/',health_check),
     path('',include('application.urls'))
 ]
